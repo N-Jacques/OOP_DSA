5d11b3d418372f212d574d7eccadaf848c3eaeb7
N-Jacques <njroxas613@gmail.com>
2024-11-25 03:43:10 +0800

    OOP_Code

---
+++ src/profile_page.py
@@ -0,0 +1,8 @@
+import time 
+def profile_page():
+    print("\nAccessing your profile...")
+    time.sleep(2)
+    print("=" * 40)
+    print("Name: Leah Nuguid")
+    print("Order History: 5 orders")
+    print("=" * 40)
