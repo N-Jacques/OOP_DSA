7c2bd14d9ee1fbf03f5f120ccfce0280f3dab395
Niccolo <njroxas613@gmail.com>
2024-12-07 05:12:03 +0800

    added comma
    ako ma-cocoma neto sa bwiset

--- src/home_page.py
+++ src/home_page.py
@@ -24,7 +24,7 @@ def home(user_id): # main function of the homepage
     cursor = user_data.cursor()
     query = "SELECT first_name FROM User WHERE user_id = ?" # get first name from database
 
-    cursor.execute(query, (user_id))
+    cursor.execute(query, (user_id,))
     result = cursor.fetchone()  # Fetch the result
     
     
